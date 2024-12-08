76fe0809bda8d4142f2c570b01aa52326b013fec
Krushna <krushnachandra.dash@walmart.com>
2024-12-08 22:03:40 +0530

    3rd commit

--- AzureScript.cmd
+++ AzureScript.cmd
@@ -7,7 +7,10 @@ az appservice plan create --name krushnaAppServicePlan --resource-group game --s
 
 az webapp create --name krushnaBalloonGame --resource-group game --plan krushnaAppServicePlan
 
-az webapp deployment source config-zip --resource-group game --name krushnaBalloonGameApp --src path/to/your/project.zip
+#az webapp deployment source config-zip --resource-group game --name krushnaBalloonGameApp --src path/to/your/project.zip
+
+az webapp deployment source config --name krushnaBalloonGame --resource-group game --repo-url https://github.com/krushnaDash/ballon-brust/ --branch main --manual-integration
+
 
 
 https://krushnaBalloonGame.azurewebsites.net
